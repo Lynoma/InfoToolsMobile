020bebe5f65a73c4105fbe86ddd39fb2cb54f560
Lynoma <85151448+Lynoma@users.noreply.github.com>
2022-05-03 16:30:55 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MobileInfoTools.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileInfoTools
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            MainPage page = new MainPage();
+            MainPage = new NavigationPage(page);
+            MainPage.BindingContext = new MainPageViewModel(MainPage);
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
